4eed0b9f984662508e545b2f9ea9854712b3dc35
padawanKardi <okardos@seznam.cz>
2023-12-10 21:24:24 +0100

    ATTEMPT

--- manualTranslatedNotesFromReport.js
+++ manualTranslatedNotesFromReport.js
@@ -64,7 +64,7 @@ translations = {
     cs_CZ: {
         unknown: "Nezjištěno",
         verifyReportPage: "Tento skript lze spustit pouze na oznámení.",
-        offensive: "OFF",
+        offensive: "OFFENSIVE",
         defensive: "DEF",
         probOffensive: "Pravděpodobně OFF",
         probDefensive: "Pravděpodobně DEF",
